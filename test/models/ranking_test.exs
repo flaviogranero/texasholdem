a9e85e3691cc67a54e3e8b15d571f4feeee22e5e
Flavio Granero <maltempe@gmail.com>
2015-11-20 16:10:37 -0200

    ranking values, combinations and best_possible_hand

--- test/models/ranking_test.exs
+++ test/models/ranking_test.exs
@@ -5,28 +5,28 @@ defmodule TexasHoldem.RankingTest do
 
   test "ranks as royal_flush" do
     cards = Enum.map 10..14, &(%Deck.Card{rank: &1, suit: :spades})
-    assert Ranking.evaluate(cards) == :royal_flush
+    assert Ranking.evaluate(cards) == Ranking.royal_flush
   end
 
   test "ranks as straight_flush" do
     cards = Enum.map 9..13, &(%Deck.Card{rank: &1, suit: :spades})
-    assert Ranking.evaluate(cards) == :straight_flush
+    assert Ranking.evaluate(cards) == Ranking.straight_flush
   end
 
   test "ranks as straight_flush with ACE" do
     cards = Enum.map(2..5, &(%Deck.Card{rank: &1, suit: :spades})) ++ [%Deck.Card{rank: 14, suit: :spades}]
 
-    assert Ranking.evaluate(cards) == :straight_flush
+    assert Ranking.evaluate(cards) == Ranking.straight_flush
   end
 
   test "ranks as four_of_a_kind" do
     cards = Enum.map(Deck.suits, &(%Deck.Card{rank: 4, suit: &1})) ++ [%Deck.Card{rank: 6, suit: :hearts}]
-    assert Ranking.evaluate(cards) == :four_of_a_kind
+    assert Ranking.evaluate(cards) == Ranking.four_of_a_kind
   end
 
   test "ranks as four_of_a_kind with lower different card" do
     cards = Enum.map(Deck.suits, &(%Deck.Card{rank: 4, suit: &1})) ++ [%Deck.Card{rank: 3, suit: :hearts}]
-    assert Ranking.evaluate(cards) == :four_of_a_kind
+    assert Ranking.evaluate(cards) == Ranking.four_of_a_kind
   end
 
   test "ranks as full_house" do    
@@ -36,7 +36,7 @@ defmodule TexasHoldem.RankingTest do
     cards_rank_6 = Deck.suits 
                     |> Enum.map(&(%Deck.Card{rank: 6, suit: &1}))
                     |> Enum.take_random(2)
-    assert Ranking.evaluate(cards_rank_4 ++ cards_rank_6) == :full_house
+    assert Ranking.evaluate(cards_rank_4 ++ cards_rank_6) == Ranking.full_house
   end
 
   test "ranks as full_house with 2 lower first" do    
@@ -46,6 +46,28 @@ defmodule TexasHoldem.RankingTest do
     cards_rank_6 = Deck.suits 
                     |> Enum.map(&(%Deck.Card{rank: 6, suit: &1}))
                     |> Enum.take_random(3)
-    assert Ranking.evaluate(cards_rank_4 ++ cards_rank_6) == :full_house
+    assert Ranking.evaluate(cards_rank_4 ++ cards_rank_6) == Ranking.full_house
+  end
+
+  test "combinations 4 elements in groups of 3" do
+    assert Ranking.combinations([1,2,3,4], 3) == [
+      [4, 3, 2], [4, 3, 1], [4, 2, 1], [3, 2, 1]
+    ]
+  end
+
+  test "best possible hand" do
+    board = [
+      %Deck.Card{rank: 14, suit: :spades},
+      %Deck.Card{rank: 14, suit: :clubs},
+      %Deck.Card{rank: 10, suit: :clubs},
+      %Deck.Card{rank: 9, suit: :spades},
+      %Deck.Card{rank: 8, suit: :clubs}
+    ]
+    hand = [
+      %Deck.Card{rank: 14, suit: :hearts},
+      %Deck.Card{rank: 14, suit: :diamonds},
+    ]
+    four_of_a_kind = Ranking.four_of_a_kind
+    {four_of_a_kind, _} = Ranking.best_possible_hand(board, hand)
   end
 end
--- web/models/ranking.ex
+++ web/models/ranking.ex
@@ -1,5 +1,27 @@
 defmodule TexasHoldem.Ranking do
 
+  def royal_flush, do:     10
+  def straight_flush, do:  9
+  def four_of_a_kind, do:  8
+  def full_house, do:      7
+  def flush, do:           6
+  def straight, do:        5
+  def three_of_a_kind, do: 4
+  def two_pair, do:        3
+  def one_pair, do:        2
+  def high_card, do:       1
+
+  def get_rank_value(name) do
+    Module.get_attribute __MODULE__, name
+  end
+
+  def best_possible_hand(board, hand) do
+    board ++ hand
+      |> combinations(5)
+      |> Stream.map(&{evaluate(&1), &1})
+      |> Enum.max
+  end
+
   def evaluate(cards) do
     cards |> Enum.map(&to_tuple/1) |> Enum.sort |> eval
   end
@@ -10,48 +32,61 @@ defmodule TexasHoldem.Ranking do
 
   defp eval(
     [{10, s}, {11, s}, {12, s}, {13, s}, {14, s}]
-  ), do: :royal_flush
+  ), do: royal_flush
 
   defp eval(
     [{a, s}, {_b, s}, {_c, s}, {_d, s}, {e, s}]
-  ) when e - a == 4, do: :straight_flush
+  ) when e - a == 4, do: straight_flush
   defp eval(
     [{2, s}, {3, s}, {4, s}, {5, s}, {14, s}]
-  ), do: :straight_flush
+  ), do: straight_flush
 
   defp eval(
-    [{_a, _}, {_a, _}, {_a, _}, {_a, _}, {_b, _}]
-  ), do: :four_of_a_kind
+    [{a, _}, {a, _}, {a, _}, {a, _}, {_b, _}]
+  ), do: four_of_a_kind
   defp eval(
-    [{_b, _}, {_a, _}, {_a, _}, {_a, _}, {_a, _}]
-  ), do: :four_of_a_kind
+    [{_b, _}, {a, _}, {a, _}, {a, _}, {a, _}]
+  ), do: four_of_a_kind
 
   defp eval(
-    [{_a, _}, {_a, _}, {_a, _}, {_b, _}, {_b, _}]
-  ), do: :full_house
+    [{a, _}, {a, _}, {a, _}, {b, _}, {b, _}]
+  ), do: full_house
   defp eval(
-    [{_b, _}, {_b, _}, {_a, _}, {_a, _}, {_a, _}]
-  ), do: :full_house
+    [{b, _}, {b, _}, {a, _}, {a, _}, {a, _}]
+  ), do: full_house
 
-  defp eval([{_e, s}, {_d, s}, {_c, s}, {_b, s}, {_a, s}]), do: :flush
+  defp eval([{_e, s}, {_d, s}, {_c, s}, {_b, s}, {_a, s}]), do: flush
 
   defp eval([{a, _}, {b, _}, {c, _}, {d, _}, {e, _}])
     when a + 1 == b and b + 1 == c and c + 1 == d and d + 1 == e,
-    do: :straight
-  defp eval([{2, _}, {3 , _}, {4 , _}, {5 , _}, {14, _}]), do: :straight
+    do: straight
+  defp eval([{2, _}, {3 , _}, {4 , _}, {5 , _}, {14, _}]), do: straight
 
-  defp eval([{a, _}, {a, _}, {a, _}, {_c, _}, {_b, _}]), do: :three_of_a_kind
-  defp eval([{_c, _}, {a, _}, {a, _}, {a, _}, {_b, _}]), do: :three_of_a_kind
-  defp eval([{_c, _}, {_b, _}, {a, _}, {a, _}, {a, _}]), do: :three_of_a_kind
+  defp eval([{a, _}, {a, _}, {a, _}, {_c, _}, {_b, _}]), do: three_of_a_kind
+  defp eval([{_c, _}, {a, _}, {a, _}, {a, _}, {_b, _}]), do: three_of_a_kind
+  defp eval([{_c, _}, {_b, _}, {a, _}, {a, _}, {a, _}]), do: three_of_a_kind
 
-  defp eval([{b, _}, {b, _}, {a, _}, {a, _}, {_c, _}]), do: :two_pair
-  defp eval([{b, _}, {b, _}, {_c, _}, {a, _}, {a, _}]), do: :two_pair
-  defp eval([{_c, _}, {b, _}, {b, _}, {a, _}, {a, _}]), do: :two_pair
+  defp eval([{b, _}, {b, _}, {a, _}, {a, _}, {_c, _}]), do: two_pair
+  defp eval([{b, _}, {b, _}, {_c, _}, {a, _}, {a, _}]), do: two_pair
+  defp eval([{_c, _}, {b, _}, {b, _}, {a, _}, {a, _}]), do: two_pair
 
-  defp eval([{a, _}, {a, _}, {_d, _}, {_c, _}, {_b, _}]), do: :one_pair
-  defp eval([{_d, _}, {a, _}, {a, _}, {_c, _}, {_b, _}]), do: :one_pair
-  defp eval([{_d, _}, {_c, _}, {a, _}, {a, _}, {_b, _}]), do: :one_pair
-  defp eval([{_d, _}, {_c, _}, {_b, _}, {a, _}, {a, _}]), do: :one_pair
+  defp eval([{a, _}, {a, _}, {_d, _}, {_c, _}, {_b, _}]), do: one_pair
+  defp eval([{_d, _}, {a, _}, {a, _}, {_c, _}, {_b, _}]), do: one_pair
+  defp eval([{_d, _}, {_c, _}, {a, _}, {a, _}, {_b, _}]), do: one_pair
+  defp eval([{_d, _}, {_c, _}, {_b, _}, {a, _}, {a, _}]), do: one_pair
 
-  defp eval([{_e, _}, {_d, _}, {_c, _}, {_b, _}, {_a, _}]), do: :high_card
+  defp eval([{_e, _}, {_d, _}, {_c, _}, {_b, _}, {_a, _}]), do: high_card
+
+  # Ported from the Erlang
+  # http://rosettacode.org/wiki/Combinations#Dynamic_Programming
+  def combinations(list, k), do: List.last(all_combinations(list, k))
+
+  defp all_combinations(list, k) do
+    accum = [[[]]] ++ List.duplicate([], k)
+    Enum.reduce list, accum, fn(x, next) ->
+      sub = Enum.take(next, length(next) - 1)
+      step = [[]] ++ (for l <- sub, do: (for s <- l, do: [x|s]))
+      :lists.zipwith(&:lists.append/2, step, next)
+    end
+  end
 end
